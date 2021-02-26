8c17b0138926f0d138b288f09387afbc7c607197
github username <github email address>
2021-02-26 17:50:04 -0500

    commit tree c01d86531bb4b72f7da86102127217425437a1ea

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({stocks, onHandleClick}) {
+  // console.log(stocks)
+  const stockList = stocks.map((stock) => {
+    return (
+      <Stock 
+        key = {stock.id}
+        id = {stock.id}
+        name = {stock.name}
+        price = {stock.price}
+        ticker = {stock.ticker}
+        type = {stock.type}
+        onHandleClick = {onHandleClick}
+        />
+    )
+  })
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stockList}
     </div>
   );
 }
